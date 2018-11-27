2e6a504f9ea2c29783f0e8758135e4373e468648
Arian R. Amiri <aamiri@ruelala.com>
2018-11-27 12:25:46 -0800

    Reboot to be more serverless * moved db from infra (can probably get rid of a lot of vpc stuff * replaced with dynamodb * rewrote agent creation to leverage dyanmodb instead of rds

--- src/lambdas.py
+++ src/lambdas.py
@@ -2,13 +2,14 @@ import logging
 
 import boto3
 
-from services import agent_management, contract_management
+from services import agent_management
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
 def create_agents(event, context):
+    logger.info(event)
     logger.info('LoadPlayers triggered')
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
@@ -16,7 +17,3 @@ def create_agents(event, context):
     agent_management.create_agents(bucket, key)
 
     return key, bucket
-
-def reset_agents(event, context):
-    agent_management.reset_agents()
-    return contract_management.assign_contracts()
--- src/services/agent_management/__init__.py
+++ src/services/agent_management/__init__.py
@@ -1,54 +1,52 @@
-from csv import DictReader
-from itertools import izip
 import logging
+import os
+
+from csv import DictReader
 
 import boto3
 
-from db_access import connection
-from services import code_name
-from services.agent_management import sql
-from services.agent_management import data_access
-
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 s3 = boto3.resource('s3')
+dynamodb = boto3.client('dynamodb')
+
 
 def create_agents(bucket, key):
     logger.info('Loading player data from s3://%s/%s', bucket, key)
     obj =  s3.Object(bucket_name=bucket, key=key)
-    lines = obj.get()['Body'].read().split('\n')
-    data = [line for line in DictReader(lines)]
+    lines = obj.get()['Body'].read().decode('utf-8').split('\n')
+    batch = []
+    for line in DictReader(lines):
+        if len(batch) == 25:
+            _create_agents_batch(batch)
+            batch = []
+        else:
+            batch.append({
+                'PutRequest': {
+                    'Item': {
+                        'slack_handle': {'S': line['slack_handle']},
+                        'name': {'S': line['agent_name']},
+                        'is_alive': {'BOOL': True},
+                        'kills': {'N': '0'},
+                        'deaths': {'N': '0'}
+                    }
+                }
+            })
 
-    logger.info('Inserting %s players', len(data))
-
-    with connection.cursor() as cursor:
-        cursor.executemany(sql.ADD_PLAYERS, data)
-    connection.commit()
+    if batch:
+        _create_agents_batch(batch)
 
     logger.info('Player load complete')
 
 
-def reset_agents():
-    """
-    First iteration:  Just assigning code names.
-
-    Need to figure out:
-        - How many agents need new code names (based on being alive)
-        - What are all the current code names
-    """
-    logger.info('Beginning player reset')
-    assassinated_players = tuple(data_access.get_assassinated_players())
-
-    if not assassinated_players:
-        logger.info('No players need to be reset')
-        return
-
-    new_codenames = code_name.generate_n_codenames(len(assassinated_players))
-    data_access.assign_codenames(izip(assassinated_players, new_codenames))
-    data_access.revive_assassinated_agents()
-    logger.info('Player reset was successful')
-
-
-def get_all_agents():
-    return data_access.get_all()
+def _create_agents_batch(agents_batch):
+    logger.info(agents_batch)
+    table_name = os.environ['PLAYER_TABLE']
+    request_items = {
+        table_name: agents_batch
+    }
+    logger.info(request_items)
+    dynamodb.batch_write_item(
+        RequestItems=request_items
+    )
